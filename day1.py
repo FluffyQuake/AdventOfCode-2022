773ea650072a5c82732392eb5b383b0df9af4631
Kaspar Vakkum <apelsin.kv@gmail.com>
2022-12-02 10:29:29 +0200

    first commit

---
+++ day1.py
@@ -0,0 +1,7 @@
+with open("day1.txt") as f:
+    cals = f.read().strip()
+elves = sorted([sum(int(c) for c in e.split("\n")) for e in cals.split("\n\n")])
+print(elves[-1])
+print(sum(elves[-3:]))
+
+# r0f1 on github :)
